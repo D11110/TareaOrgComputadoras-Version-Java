4cf9ce968a7372a66d226bb9bd1e43a51326afa5
Andres Nuila <enuila634@gmail.com>
2021-12-06 20:22:03 -0600

    Avance realizado con el inge

--- src/simulacion_tiempo_cache/Simulacion_Tiempo_Cache.java
+++ src/simulacion_tiempo_cache/Simulacion_Tiempo_Cache.java
@@ -1,21 +1,57 @@
 package simulacion_tiempo_cache;
 
+import java.util.ArrayList;
+
 public class Simulacion_Tiempo_Cache {
 
     RAM ram = new RAM();
+    ArrayList<Integer> datosEnRam = new ArrayList<>();
     Cache cache = new Cache();
+    boolean filaValidar[] = cache.getValidar();
+    int filaEtiqueta[] = cache.getEtiqueta();
+    boolean filaMod[] = cache.getMod();
+    int datos[][] = cache.getValores();
+    double tiempo = 0.0;
 
     public static void main(String[] args) {
-        double tiempo = 0.0;
 
     }
 
     public int leer(int posicion) {
         int dato = 0;
-        boolean filaValidar[] = cache.getValidar();
-        int filaEtiqueta[] = cache.getEtiqueta();
-        if (filaValidar[posicion]) {
-            
+        int etiqueta = posicion / 64;
+        int bloque = posicion / 8;
+        int linea = bloque % 8;
+        int palabra = posicion % 8;
+        if (filaValidar[etiqueta]) {
+            if (filaEtiqueta[linea] == etiqueta) {
+                dato = datos[linea][palabra];
+                tiempo += 0.01;
+            } else if (filaMod[linea]) {
+                for (int i = (filaEtiqueta[linea] * 64) + (linea * 8), j = 0; i <= (bloque * 8) + 7; i++, j++) {
+                    datosEnRam.add(datos[linea][j]);
+                }
+                for (int i = (bloque * 8), j = 0; i <= (bloque * 8) + 7; i++, j++) {
+                    datos[linea][j] = datosEnRam.get(i);
+                }
+                dato = datos[linea][palabra];
+                filaMod[linea] = false;
+                tiempo += 0.11;
+            } else {
+                for (int i = (bloque * 8), j = 0; i <= (bloque * 8) + 7; i++, j++) {
+                    datos[linea][j] = datosEnRam.get(i);
+                }
+                dato = datos[linea][palabra];
+                filaMod[linea] = false;
+                tiempo += 0.11;
+            }
+        } else {
+            filaValidar[linea] = true;
+            for (int i = (bloque * 8), j = 0; i <= (bloque * 8) + 7; i++, j++) {
+                datos[linea][j] = datosEnRam.get(i);
+            }
+            dato = datos[linea][palabra];
+            tiempo += 0.11;
         }
         return dato;
     }
